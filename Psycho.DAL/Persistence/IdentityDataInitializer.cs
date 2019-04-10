3fd0270120f9301ead57046c9a6272f6784dd73c
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2019-04-10 10:37:00 +0300

    Closed #5. Added IdentityDataInitializer.

---
+++ Psycho.DAL/Persistence/IdentityDataInitializer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Psycho.DAL.Core.Domain;
+
+namespace Psycho.DAL.Persistence
+{
+    public static class IdentityDataInitializer
+    {
+        public static async Task SeedData(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            await SeedRoles(roleManager);
+            await SeedUsers(userManager);
+        }
+
+        public static async Task SeedUsers(UserManager<User> userManager)
+        {
+            IdentityResult userResult;
+            const string GeneralPassword = "123";
+            List<User> users = new List<User>();
+            users.Add(new User() { UserName = "lysyshyn@gmail.com", FirstName = "Admin", LastName = "Admin", Email = "lysyshyn@gmail.com", EmailConfirmed = true, Blocked = false, RoleId = 1 });
+            users.Add(new Psychologist() { UserName = "komarov@gmail.com", FirstName = "Doctor", LastName = "Komarov", Email = "komarov@gmail.com", EmailConfirmed = true, Blocked = false, RoleId = 2 });
+            users.Add(new Psychologist() { UserName = "kushnir@gmail.com", FirstName = "Doctor", LastName = "Kushnir", Email = "kushnir@gmail.com", EmailConfirmed = true, Blocked = false, RoleId = 2 });
+            users.Add(new Psychologist() { UserName = "zavadska@gmail.com", FirstName = "Doctor", LastName = "Zavadska", Email = "zavadska@gmail.com", EmailConfirmed = true, Blocked = false, RoleId = 2 });
+            foreach (var user in users)
+            {
+                var userExist = await userManager.FindByEmailAsync(user.Email);
+                if (userExist == null)
+                {
+                    userResult = await userManager.CreateAsync(user, GeneralPassword);
+                    var add_role = await userManager.AddToRoleAsync(user, user.Role.Name);
+                }
+            }
+        }
+
+        public static async Task SeedRoles(RoleManager<Role> roleManager)
+        {
+            string[] roleNames = { "Admin", "Psychologist", "AuthorizedUser", "AnonymousUser" };
+            IdentityResult roleResult;
+            foreach (var role in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new Role(role));
+                }
+            }
+        }
+    }
+}
